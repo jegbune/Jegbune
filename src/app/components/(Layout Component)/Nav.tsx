bfdcae9aed9eaea256e3fbd301085b30aa07d22c
Jeremiah Egbune <jegbune1@gmail.com>
2024-10-19 12:49:55 +0100

    build runtime error corrected

--- src/app/components/(Layout Component)/Nav.tsx
+++ src/app/components/(Layout Component)/Nav.tsx
@@ -2,13 +2,13 @@
 
 import Link from 'next/link'
 import { useState } from 'react'
-import { AiFillFacebook, AiFillGithub, AiFillLinkedin, AiOutlineFacebook, AiOutlineMenu } from 'react-icons/ai'
+import { AiFillFacebook, AiFillGithub, AiFillLinkedin, AiOutlineMenu } from 'react-icons/ai'
 
 
 export const SocialList = () => (
   <div className='flex items-center gap-4'>
-    <Link href='https://github.com/jegbune' target='_blank'><AiFillGithub className='text-3xl text-green-500 rounded-full' /></Link>
-    <Link href='https://www.linkedin.com/in/jeremiah-ikechukwu-egbune-98b0a1178/' target='_blank'><AiFillLinkedin className='text-3xl text-green-500 rounded-full' /></Link>
+    <Link href='https://github.com/jegbune' target='_blank'><AiFillGithub className='text-3xl text-green-600 rounded-full' /></Link>
+    <Link href='https://www.linkedin.com/in/jeremiah-ikechukwu-egbune-98b0a1178/' target='_blank'><AiFillLinkedin className='text-3xl text-green-600 rounded-full' /></Link>
     <Link href='https://www.facebook.com/jeremiah.egbune' target='_blank'><AiFillFacebook className='text-3xl text-green-600 rounded-full' /></Link>
   </div>
 )
